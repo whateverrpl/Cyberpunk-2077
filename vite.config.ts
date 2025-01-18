54a3fd8d564f9c29f5df6ae41c66f70f40add161
whateverrpl <whateverPL@protonmail.com>
2025-01-18 01:47:48 +0100

    fixed architecture

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+          output: {
+            // Настраиваем пути для ассетов (включая шрифты)
+            assetFileNames: (assetInfo) => {
+              if (/\.woff2?$/.test(assetInfo.name || '')) {
+                return 'assets/fonts/[name][extname]';
+              }
+              return 'assets/[name][extname]';
+            },
+          },
+        },
+      },
     base: '/Cyberpunk-2077/'
-})
+});
